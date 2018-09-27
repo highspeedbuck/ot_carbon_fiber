9941dbb8807a44e242018490cf793ca3ba5afe15
highspeedbuck <tanjunlang@qq.com>
2018-09-27 20:57:45 +0800

    update the index framework

--- ot_carbon/src/router.js
+++ ot_carbon/src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import Test from './routes/Test';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/test" exact component={Test} />
       </Switch>
     </Router>
   );
